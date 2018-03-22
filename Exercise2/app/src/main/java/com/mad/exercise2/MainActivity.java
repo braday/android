52d92e856dd145de306feba49ace0cc78d9a1cf0
braday <gamyuen@hotmail.com>
2018-03-22 12:31:28 +1100

    add tut to github

---
+++ Exercise2/app/src/main/java/com/mad/exercise2/MainActivity.java
@@ -0,0 +1,21 @@
+package com.mad.exercise2;
+
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import static com.mad.exercise2.R.drawable.android_logo;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
